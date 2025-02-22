6bb915e8afbb15054d02882b5a2554c3412789d9
zhangxiaomeng <921996026@qq.com>
2025-02-22 16:05:16 +0800

    优化

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.post('/chat', async (req, res) => {
                         }
                     } catch (e) {
                         // 忽略解析错误
+                        currentText += '网络错误';
                     }
                 }
             }
@@ -81,6 +82,7 @@ app.post('/chat', async (req, res) => {
                         }
                     } catch (e) {
                         // 忽略解析错误
+                        currentText += '网络错误';
                     }
                 }
             }
