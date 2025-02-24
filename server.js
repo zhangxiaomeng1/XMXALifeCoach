58784ee969fefac8ad3169bf9609f1ec2938ae36
zhangxiaomeng <921996026@qq.com>
2025-02-24 19:28:18 +0800

    端口

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const API_KEY = process.env.API_KEY;
 const API_URL = process.env.API_URL;
